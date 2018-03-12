84b8f3fbccb502caaa2f21dda1afc3c1315f5102
Anton Fedorov <antonfallstars@gmail.com>
2018-03-12 21:05:29 +0700

    Some change2!

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,7 +15,7 @@
                             <p>Email: {{$user->email}}</p>
                             <p>Name: {{$user->name}}</p>
                             <p>Surname: {{$user->surname}}</p>
-                            <p>Role: </p>
+                            <p>Role:  </p>
                             @can('update-user', $user)
                                 <a class="btn btn-small btn-success" href="{{ route('edit_user', $user->id) }}">Edit
                                     profile</a>
