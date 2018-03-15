8d01c0ed8b356c3c802c71126618cba8c5d1736d
Anton Fedorov <antonfallstars@gmail.com>
2018-03-15 15:14:42 +0700

    Realize reset password system.

--- app/User.php
+++ app/User.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements \Illuminate\Contracts\Auth\Authenticatable
+class User extends Model implements AuthenticatableContract,CanResetPasswordContract
 {
-    use Authenticatable;
+    use Authenticatable, CanResetPassword, Notifiable;
 
     protected $table = 'users';
     protected $guarded = [];
